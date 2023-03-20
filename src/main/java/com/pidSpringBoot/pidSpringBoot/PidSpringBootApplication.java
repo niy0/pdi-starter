60073e1431e230549069560ec9d7ae965191f2f0
Niyoyita <niyo.roger@hotmail.com>
2023-03-20 18:46:41 +0100

    first commit

---
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/PidSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pidSpringBoot.pidSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidSpringBootApplication.class, args);
+	}
+
+}
