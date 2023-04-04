2413ced2ce1993c8e98358f1fde4e99b71676f19
Denis <bidjada1102@gmail.com>
2023-04-04 14:04:02 +0200

    Relation ManyToMany between User & Representation

---
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/config/ThymeleafConfiguration.java
@@ -0,0 +1,13 @@
+package com.pidSpringBoot.pidSpringBoot.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThymeleafConfiguration {
+    @Bean
+    public LayoutDialect thymeleafDialect() {
+        return new LayoutDialect();
+    }
+}
---
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/model/RepresentationRepository.java
@@ -0,0 +1,12 @@
+package com.pidSpringBoot.pidSpringBoot.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface RepresentationRepository extends CrudRepository<Representation, Long> {
+   // List<Representation> findByShow(Show show);
+  //  List<Representation> findByLocation(Location location);
+    List<Representation> findByWhen(LocalDateTime when);
+}
