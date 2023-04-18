f1d78f57460e0c2abda8ee8ceb52525afc4e817d
MariaMorales1994 <claremellamorales@yahoo.com>
2023-04-18 19:30:51 +0200

    Relation ManyToMany between Artist & Type

--- src/main/java/com/pidSpringBoot/pidSpringBoot/model/Artist.java
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/model/Artist.java
@@ -6,6 +6,9 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @Entity
@@ -16,4 +19,28 @@ public class Artist {
     private Long id;
     private String firstname;
     private String lastname;
+
+    @ManyToMany(mappedBy = "artists")
+    private List<Type> types = new ArrayList<>();
+
+    public List<Type> getTypes(){
+        return types;
+    }
+
+    public Artist addType(Type type) {
+        if (!this.types.contains(type)) {
+            this.types.add(type);
+            type.addArtist(this);
+        }
+
+        return this;
+    }
+
+        public Artist removeType(Type type){
+        if (!this.types.contains(type)){
+            this.types.remove(type);
+            type.getArtists().remove(this);
+        }
+        return this;
+    }
 }
--- src/main/java/com/pidSpringBoot/pidSpringBoot/model/Type.java
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/model/Type.java
@@ -2,6 +2,9 @@ package com.pidSpringBoot.pidSpringBoot.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="types")
 public class Type {
@@ -9,6 +12,13 @@ public class Type {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String type;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artist_type",
+            joinColumns = @JoinColumn(name = "type_id"),
+            inverseJoinColumns =  @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
     protected Type() { }
 
     public Type(String type) {
@@ -23,9 +33,28 @@ public class Type {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public List<Artist> getArtists() {
+        return artists;
     }
+
+    public Type addArtist(Artist artist) {
+        if(!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this);
+        }
+
+        return this;
+    }
+
+    public Type removeType(Artist artist) {
+        if(this.artists.contains(artist)) {
+            this.artists.remove(artist);
+            artist.getTypes().remove(this);
+        }
+
+        return this;
+    }
+
 
     @Override
     public String toString() {
