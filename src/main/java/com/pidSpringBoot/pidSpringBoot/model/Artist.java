02b6b678cd30869bd2e1445a67fccb9db420cea9
MariaMorales1994 <claremellamorales@yahoo.com>
2023-04-23 18:58:48 +0200

    Form create Artist

--- src/main/java/com/pidSpringBoot/pidSpringBoot/model/Artist.java
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/model/Artist.java
@@ -2,6 +2,8 @@ package com.pidSpringBoot.pidSpringBoot.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,18 +12,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
+@NoArgsConstructor(force = true, access = AccessLevel.PUBLIC)
 @Entity
 @Table(name="artists")
 public class Artist {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotEmpty(message = "The firstname must not be empty.")
+    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String firstname;
+    @NotEmpty(message = "The lastname must not be empty.")
+    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String lastname;
 
     @ManyToMany(mappedBy = "artists")
     private List<Type> types = new ArrayList<>();
+
+    public Artist(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 
     public List<Type> getTypes(){
         return types;
