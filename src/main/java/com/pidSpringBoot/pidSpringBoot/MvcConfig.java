16f33729ba297f0fbbaaef0aee3d525eea6e2839
deb060794 <128077215+deb060794@users.noreply.github.com>
2023-05-17 12:09:24 +0200

    Update MvcConfig.java

--- src/main/java/com/pidSpringBoot/pidSpringBoot/MvcConfig.java
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/MvcConfig.java
@@ -3,6 +3,8 @@ package com.pidSpringBoot.pidSpringBoot;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -11,4 +13,10 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/admin/home").setViewName("admin_home");
         registry.addViewController("/user/member_home").setViewName("member_home");
     }
+    
+    @Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+   	 return new HiddenHttpMethodFilter();
+	}
+	
 }
