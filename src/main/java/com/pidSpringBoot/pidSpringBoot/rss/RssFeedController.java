1b2eb785dad66a3fd38dfc3028f022b8212c04eb
debrah tinsia <dtinsia@gmail.com>
2023-05-19 13:54:56 +0000

    rss feed

--- src/main/java/com/pidSpringBoot/pidSpringBoot/rss/RssFeedController.java
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/rss/RssFeedController.java
@@ -1,16 +1,55 @@
 package com.pidSpringBoot.pidSpringBoot.rss;
+
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pidSpringBoot.pidSpringBoot.show.Show;
+import com.pidSpringBoot.pidSpringBoot.show.ShowRepository;
+import com.pidSpringBoot.pidSpringBoot.show.ShowService;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.feed.synd.SyndFeedImpl;
+import com.rometools.rome.io.SyndFeedOutput;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
 public class RssFeedController {
-
     @Autowired
     private RssFeedView view;
+
+    @Autowired
+    private ShowService service;
     
     @GetMapping("/rss")
-    public RssFeedView getFeed() {
-        return view;
+    public void generateRssFeed(HttpServletResponse response) throws Exception {
+        // Retrieve the list of shows from your data source
+        List<Show> shows = service.getAll();
+    
+        // Create the RSS feed
+        SyndFeed feed = new SyndFeedImpl();
+        feed.setFeedType("rss_2.0");
+        feed.setTitle("My Website RSS Feed");
+        feed.setDescription("Latest updates from My Website");
+        feed.setLink("https://laughable-soap-production.up.railway.app/");
+        feed.setPublishedDate(new Date());
+    
+        // Build the feed items
+        List<SyndEntry> entries =   view.buildFeedItems(shows);
+        feed.setEntries(entries);
+    
+        // Write the feed to the response
+        response.setContentType(MediaType.APPLICATION_XML_VALUE);
+        SyndFeedOutput output = new SyndFeedOutput();
+        output.output(feed, response.getWriter());
     }
+    
+
+
+
 }
--- src/main/java/com/pidSpringBoot/pidSpringBoot/rss/RssFeedView.java
+++ src/main/java/com/pidSpringBoot/pidSpringBoot/rss/RssFeedView.java
@@ -1,38 +1,44 @@
 package com.pidSpringBoot.pidSpringBoot.rss;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
 
-import com.rometools.rome.feed.rss.Channel;
-import com.rometools.rome.feed.rss.Item;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.pidSpringBoot.pidSpringBoot.show.Show;
+
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndContentImpl;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
 
 @Component
-public class RssFeedView extends AbstractRssFeedView {
+public class RssFeedView  {
 
-    @Override
-    protected void buildFeedMetadata(Map<String, Object> model, 
-      Channel feed, HttpServletRequest request) {
-        feed.setTitle("Projet reservation");
-        feed.setDescription("Projet des reservations des spectacles");
-        feed.setLink("https://laughable-soap-production.up.railway.app/");
+    protected List<SyndEntry> buildFeedItems(List<Show> shows) {
+        List<SyndEntry> entries = new ArrayList<>();
+    
+        for (Show show : shows) {
+            SyndEntry entry = new SyndEntryImpl();
+            entry.setTitle(show.getTitle());
+            entry.setLink("https://laughable-soap-production.up.railway.app/shows/" + show.getId());
+            entry.setPublishedDate(Date.from(Instant.now()));
+    
+            SyndContent description = new SyndContentImpl();
+            description.setValue(show.getDescription());
+            entry.setDescription(description);
+    
+            entries.add(entry);
+        }
+    
+        return entries;
     }
 
-    @Override
-    protected List<Item> buildFeedItems(Map<String, Object> model, HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-        Item entryOne = new Item();
-        entryOne.setTitle("Projet reservation");
-        entryOne.setLink("https://laughable-soap-production.up.railway.app/");
-        entryOne.setPubDate(Date.from(Instant.parse("2017-12-19T00:00:00Z")));
-        return Arrays.asList(entryOne);
-    }
+
+    
 }
